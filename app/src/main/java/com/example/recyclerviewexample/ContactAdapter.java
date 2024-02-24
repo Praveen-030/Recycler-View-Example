11d5e33f01317c53d512dbbe799361f728023b6f
Praveen-030 <praveen45625@gmail.com>
2024-02-24 18:05:56 +0530

    add Update and Delete options

--- app/src/main/java/com/example/recyclerviewexample/ContactAdapter.java
+++ app/src/main/java/com/example/recyclerviewexample/ContactAdapter.java
@@ -1,11 +1,18 @@
 package com.example.recyclerviewexample;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -33,6 +40,72 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.viewHold
         holder.name.setText(list.get(position).name);
         holder.number.setText(list.get(position).number);
         holder.img.setImageResource(list.get(position).img);
+
+        holder.llROw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.add_update_contacts);
+                EditText etName = dialog.findViewById(R.id.etName);
+                EditText etNumber = dialog.findViewById(R.id.etNumber);
+                Button btnUpdate = dialog.findViewById(R.id.btnUpdate);
+                TextView tvTitle = dialog.findViewById(R.id.tvaddUpdate);
+                tvTitle.setText("Update Contacts");
+                etName.setText(list.get(position).name);
+                etNumber.setText(list.get(position).number);
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String name="",number="";
+                        if(!etName.getText().toString().equals(""))
+                        {
+                            name = etName.getText().toString();
+                        }else {
+                            Toast.makeText(context,"Please enter Name",Toast.LENGTH_LONG).show();
+                        }
+                        if(!etNumber.getText().toString().equals(""))
+                        {
+                            number = etNumber.getText().toString();
+                        }else {
+                            Toast.makeText(context,"Please enter Number",Toast.LENGTH_LONG).show();
+                        }
+                        // set means changing data at pertucular index
+                        list.set(position,new ContactModel(list.get(position).img,name,number));
+                        //
+                        notifyItemChanged(position);
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+
+        holder.llROw.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                        .setTitle("Delete Contact")
+                        .setMessage("Are you sure want to delete?")
+                        .setIcon(R.drawable.delete)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                list.remove(position);
+                                notifyItemRemoved(position);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Alter dialog box auto,atically dismiss on click of yes or no
+                            }
+                        });
+                builder.show();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -43,11 +116,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.viewHold
     public class viewHolder extends RecyclerView.ViewHolder{
         TextView name,number;
         ImageView img;
+        LinearLayout llROw;
         public viewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.tvContactName);
             number = itemView.findViewById(R.id.tvContactNumber);
             img = itemView.findViewById(R.id.imgContect);
+            llROw = itemView.findViewById(R.id.llRow);
 
         }
     }
--- app/src/main/java/com/example/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.java
@@ -1,24 +1,71 @@
 package com.example.recyclerviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    ContactAdapter adapter;
+    FloatingActionButton add;
     ArrayList<ContactModel> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add = findViewById(R.id.floatAdd);
         recyclerView = findViewById(R.id.recyclerContacts);
         //setting up layout style
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         list = new ArrayList<>();
+
+        //updating contacts
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.add_update_contacts);
+                EditText etName = dialog.findViewById(R.id.etName);
+                EditText etNumber = dialog.findViewById(R.id.etNumber);
+                Button btnUpdate = dialog.findViewById(R.id.btnUpdate);
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String name="",number="";
+                        if(!etName.getText().toString().equals(""))
+                        {
+                            name = etName.getText().toString();
+                        }else {
+                            Toast.makeText(MainActivity.this,"Please enter Name",Toast.LENGTH_LONG).show();
+                        }
+                        if(!etNumber.getText().toString().equals(""))
+                        {
+                            number = etNumber.getText().toString();
+                        }else {
+                            Toast.makeText(MainActivity.this,"Please enter Number",Toast.LENGTH_LONG).show();
+                        }
+
+                        list.add(new ContactModel(R.drawable.f,name,number));
+                        adapter.notifyItemInserted(list.size()-1);
+                        recyclerView.scrollToPosition(list.size()-1);
+
+                        dialog.dismiss();
+                    }
+                });dialog.show();
+            }
+        });
 
         list.add(new ContactModel(R.drawable.a,"Praveen","9876543210"));
         list.add(new ContactModel(R.drawable.b,"Aman","1234567890"));
@@ -46,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         list.add(new ContactModel(R.drawable.h,"Arya","9567845674"));
 
         // calling adapter class constructor
-        ContactAdapter adapter = new ContactAdapter(this,list);
+        adapter = new ContactAdapter(this,list);
         recyclerView.setAdapter(adapter);
     }
 }
